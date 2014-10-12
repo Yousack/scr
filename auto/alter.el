04799b60fbecbae9f79e69730d5461ff320bc705
Yusaku Inoue <love.the.quiz.yousack@gmail.com>
2014-10-12 23:12:21 +0900

    概ね半分が終わった

--- auto/alter.el
+++ auto/alter.el
@@ -4,7 +4,13 @@
      "sec-1"
      "sec-2"
      "sec-2-1"
-     "sec-2-2")
+     "sec-2-2"
+     "sec-2-3"
+     "sec-2-4"
+     "sec-2-5"
+     "sec-2-6"
+     "sec-2-7"
+     "sec-2-8")
     (TeX-run-style-hooks
      "hyperref"
      "amssymb"
